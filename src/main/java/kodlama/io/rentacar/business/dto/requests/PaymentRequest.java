1932108e26183cc621bed5712d68e4946448a93a
Barış <89024790+Brsyrdkl@users.noreply.github.com>
2023-04-26 19:31:25 +0300

    Added new constants and an annotation

--- src/main/java/kodlama/io/rentacar/business/dto/requests/PaymentRequest.java
+++ src/main/java/kodlama/io/rentacar/business/dto/requests/PaymentRequest.java
@@ -24,15 +24,14 @@ public class PaymentRequest { // Base class
     @Length(min = 5)
     private String cardHolder;
 
-    @NotNull
     @Min(value = 2023)
     private int cardExpirationYear;
 
-    @NotNull
     @Max(value = 12)
     @Min(value = 1)
     private int cardExpirationMonth;
 
+    @NotNull
     @NotBlank
     @Length(min = 3, max = 3)
     private String cardCvv;
